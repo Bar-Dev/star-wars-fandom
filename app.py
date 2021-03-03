543617d2ed1f8024c150881ff5d5a82c996b155d
Bar-Dev <bdprojectservices@gmail.com>
2021-03-03 13:41:20 +0000

    Add Profile base page

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def register():
         mongo.db.users.insert_one(register)
 
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful")   
+        flash("Registration Successful") 
+        return redirect(url_for("profile", username=session["user"]))  
     return render_template("register.html")
 
 
@@ -59,8 +60,12 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    side = mongo.db.users.find_one({"username": session["user"]})["sides"]
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    side = mongo.db.users.find_one(
+                        {"username": session["user"]})["sides"]
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -72,6 +77,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
